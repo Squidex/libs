e88170996f462aec103de3b5a55faa337e2215f3
Sebastian <sebastian@squidex.io>
2023-03-25 19:10:25 +0100

    Test

--- hosting/Squidex.Hosting/Web/HtmlTransformMiddleware.cs
+++ hosting/Squidex.Hosting/Web/HtmlTransformMiddleware.cs
@@ -5,8 +5,13 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using System.Threading;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 
 namespace Squidex.Hosting.Web;
 
@@ -21,56 +26,134 @@ public sealed class HtmlTransformMiddleware
         this.next = next;
     }
 
+    private sealed class BufferStream : Stream
+    {
+        private readonly Stream inner;
+        private MemoryStream? memoryStream;
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => ActualStream.Length;
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        private Stream ActualStream
+        {
+            get => memoryStream ?? inner;
+        }
+
+        public BufferStream(Stream inner)
+        {
+            this.inner = inner;
+        }
+
+        public void Buffer()
+        {
+            memoryStream = new MemoryStream();
+        }
+
+        public async Task CompleteAsync(HtmlTransformOptions options, HttpContext context,
+            CancellationToken cancellationToken)
+        {
+            if (memoryStream != null)
+            {
+                var html = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+                if (options.AdjustBase)
+                {
+                    html = html.AdjustBase(context);
+                }
+
+                if (options.Transform != null)
+                {
+                    html = await options.Transform(html, context);
+                }
+
+                var bytes = Encoding.UTF8.GetBytes(html);
+
+                await inner.WriteAsync(bytes, cancellationToken);
+
+                memoryStream = new MemoryStream();
+            }
+            else
+            {
+                await inner.FlushAsync(cancellationToken);
+            }
+        }
+
+        public override Task FlushAsync(
+            CancellationToken cancellationToken)
+        {
+            return ActualStream.FlushAsync(cancellationToken);
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count,
+            CancellationToken cancellationToken)
+        {
+            return ActualStream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer,
+            CancellationToken cancellationToken = default)
+        {
+            return ActualStream.WriteAsync(buffer, cancellationToken);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
     public async Task InvokeAsync(HttpContext context)
     {
         var response = context.Response;
 
-        var responseBuffer = new MemoryStream();
-        var responseBody = response.Body;
+        var bufferStream = new BufferStream(response.Body);
 
-        response.Body = responseBuffer;
-        try
+        response.Body = bufferStream;
+        response.OnStarting(() =>
         {
-            await next(context);
-        }
-        finally
-        {
-            response.Body = responseBody;
-        }
+            if (response.ContentType?.StartsWith("text/html", StringComparison.OrdinalIgnoreCase) != true)
+            {
+                return Task.CompletedTask;
+            }
 
-        // Nothing needs to be written here.
-        if (responseBuffer.Length == 0 || response.StatusCode == StatusCodes.Status304NotModified)
+            bufferStream.Buffer();
+            return Task.CompletedTask;
+        });
+
+        await next(context);
+
+        if (bufferStream.Length == 0 || response.StatusCode == StatusCodes.Status304NotModified)
         {
             return;
         }
 
-        // We need to change the content length header.
-        if (!response.HasStarted && response.ContentType?.StartsWith("text/html", StringComparison.OrdinalIgnoreCase) == true)
-        {
-            var html = Encoding.UTF8.GetString(responseBuffer.ToArray());
-
-            if (options.AdjustBase)
-            {
-                html = html.AdjustBase(context);
-            }
-
-            if (options.Transform != null)
-            {
-                html = await options.Transform(html, context);
-            }
-
-            var bytes = Encoding.UTF8.GetBytes(html);
-
-            // Change the content length in case the transformation has added chars.
-            response.ContentLength = bytes.Length;
-
-            await response.BodyWriter.WriteAsync(bytes, context.RequestAborted);
-        }
-        else
-        {
-            responseBuffer.Position = 0;
-
-            await responseBuffer.CopyToAsync(responseBody, context.RequestAborted);
-        }
+        await bufferStream.CompleteAsync(options, context, context.RequestAborted);
     }
 }
