55bbdfc3b90d0a4c06c1e0cd53f49aab4a99f0e0
Sebastian <sebastian@squidex.io>
2023-06-26 20:21:44 +0200

    Change registration.

--- text/Squidex.Text/ChatBots/ChatBotServiceExtensions.cs
+++ text/Squidex.Text/ChatBots/ChatBotServiceExtensions.cs
@@ -5,6 +5,7 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Squidex.Text.ChatBots;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -13,8 +14,7 @@ public static class ChatBotServiceExtensions
 {
     public static IServiceCollection AddChatBot(this IServiceCollection services)
     {
-        services.AddSingletonAs<ChatBot>()
-            .As<IChatBot>().AsSelf();
+        services.TryAddSingleton<IChatBot, ChatBot>();
 
         return services;
     }
