24d01d898a3e786f8d9f7d6b34eb9d8f3270b503
Sebastian <sebastian@squidex.io>
2023-07-14 10:40:23 +0200

    Fix docker.

--- docker-build.ps1
+++ docker-build.ps1
@@ -5,7 +5,12 @@ $version_major = $version.split(".",3)[0]
 
 Write-Output "Version $version, Major Version: $version_major"
 
-docker build . -t squidex/resizer:latest -t squide/resizer:$version -t squide/resizer:$version_major
+docker build -t resizer .
+
+docker tag resizer squidex/resizer:latest
+docker tag resizer squidex/resizer:$version
+docker tag resizer squidex/resizer:$version_major
+
 docker push squidex/resizer:latest
 docker push squidex/resizer:$version
 docker push squidex/resizer:$version_major
