7aa51021fa4b66e5b89ee1a8fc8b5f4c1b20dc67
Sebastian Stehle <sebastian@squidex.io>
2025-05-04 13:31:05 +0200

    Fix tests

--- events/Squidex.Events.EntityFramework/EFSchema.cs
+++ events/Squidex.Events.EntityFramework/EFSchema.cs
@@ -18,7 +18,7 @@ public static class EFSchema
             b.HasIndex(x => new { x.EventStream, x.EventStreamOffset }).IsUnique();
             b.HasIndex(x => new { x.EventStream, x.Position });
             b.HasIndex(x => new { x.EventStream, x.Timestamp });
-            b.Property(x => x.EventStream).HasMaxLength(2000);
+            b.Property(x => x.EventStream).HasMaxLength(1000);
         });
 
         modelBuilder.Entity<EFPosition>();
